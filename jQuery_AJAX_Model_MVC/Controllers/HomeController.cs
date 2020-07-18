65ec052e0d8101ccdebc39b1ee6aec47a30ae346
hassanrehman01398 <hassanrehman01398@gmail.com>
2020-07-18 20:53:21 +0500

    Mvc Example

---
+++ jQuery_AJAX_Model_MVC/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using jQuery_AJAX_Model_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jQuery_AJAX_Model_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult AjaxMethod(PersonModel person)
+        {
+            string name = person.taskname;
+
+            string name1 = person.Name;
+            return Json(person);
+        }
+    }
+}
---
+++ jQuery_AJAX_Model_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="jQuery_AJAX_Model_MVC.MvcApplication" Language="C#" %>
